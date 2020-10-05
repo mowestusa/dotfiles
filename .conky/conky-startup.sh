40716126307d22ffad4b4b9232b74074008fd099
mowest <mowestusa@yahoo.com>
2020-10-04 23:19:24 -0400

    Updated to MXFB 2.3 changed theming and restored my keybindings.

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -3,7 +3,7 @@
 if [ x"${DESKTOP_SESSION##*/}" = x"fluxbox" ]; then 
    sleep 20s
    killall conky
-   cd "$HOME/.conky/MX-APInfo"
-   conky -c "$HOME/.conky/MX-APInfo/AP-info-debianUpdates-coloursMod" &
+   cd "$HOME/.conky/MX-antiX"
+   conky -c "$HOME/.conky/MX-antiX/MX-antiX17" &
    exit 0
 fi
