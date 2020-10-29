fffed5dc9adf598c02f71a01def72530c99453cc
mowest <mowestusa@yahoo.com>
2020-10-28 20:18:58 -0400

    Changed Conky configuration.

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -3,7 +3,7 @@
 if [ x"${DESKTOP_SESSION##*/}" = x"fluxbox" ]; then 
    sleep 20s
    killall conky
-   cd "$HOME/.conky/MX-antiX"
-   conky -c "$HOME/.conky/MX-antiX/MX-antiX17" &
+   cd "$HOME/.conky/MX-Conkyglass"
+   conky -c "$HOME/.conky/MX-Conkyglass/conkys" &
    exit 0
 fi
